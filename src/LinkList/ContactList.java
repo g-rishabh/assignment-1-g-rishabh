3acef46a76159b24c33b47248d97b5d5fde197b3
rishabh <garg.rishabh@research.iiit.ac.in>
2020-02-24 13:05:36 +0530

    contactList class getter setter and constructor created and commit

---
+++ src/LinkList/ContactList.java
@@ -0,0 +1,31 @@
+package LinkList;
+
+public class ContactList {
+    private Node head;
+
+    public ContactList()
+    {
+        head = null;
+    }
+
+    public Node getHead()
+    {
+        return head;
+    }
+
+    public void setHead(Node head)
+    {
+        this.head = head;
+    }
+
+
+    private boolean isEmpty() {
+        boolean response=false;
+        if(head == null)
+        {
+            response=true;
+        }
+        return response;
+    }
+
+}
